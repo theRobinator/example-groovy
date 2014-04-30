1abbfb1a885d9e072e134b3e98a7695be19612a0
Robin Keller <robink@zoosk.com>
2014-04-30 14:38:57 -0700

    Make the main program happier

--- src/Main.groovy
+++ src/Main.groovy
@@ -6,7 +6,7 @@ import static Sum.sum
 def name = "Matthew"
 int programmingPoints = 10
 
-println "Hello ${name}"
+println "Hello ${name} :D"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
